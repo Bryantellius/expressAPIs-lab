000b916ca1096dae5ec8c70f7ffaa3906571c4f6
Ben Bryant <brbryant2639@gmail.com>
2020-03-24 19:01:38 -0500

    added express static route to client folder

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const apiRoutes = require("./routes");
 
@@ -8,5 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRoutes);
+
+app.use(express.static(path.join(__dirname, "../client")));
 
 app.listen(3000);
