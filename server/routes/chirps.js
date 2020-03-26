e6abbfadcefbb73dcfd980b3ed411a91e7a75a89
Ben Bryant <brbryant2639@gmail.com>
2020-03-26 10:32:17 -0500

    added try/catch into api requests, better error output

--- server/routes/chirps.js
+++ server/routes/chirps.js
@@ -2,28 +2,52 @@ const express = require("express");
 const chirpsStore = require("../chirpstore");
 let router = express.Router();
 
-router.get("/:id?", (req, res) => {
-  let id = req.params.id;
-  if (id) {
-    res.json(chirpsStore.GetChirp(id));
-  } else {
-    res.json(chirpsStore.GetChirps());
+router.get("/:id?", async (req, res) => {
+  try {
+    let id = req.params.id;
+    if (id) {
+      res.json(chirpsStore.GetChirp(id));
+    } else {
+      res.json(chirpsStore.GetChirps());
+    }
+  } catch (error) {
+    console.log(error);
+    res.status("Error attempting to GET");
   }
 });
 
 router.post("/", (req, res) => {
-  chirpsStore.CreateChirp(req.body);
-  res.sendStatus(200);
+  try {
+    let chirp = req.body;
+    chirpsStore.CreateChirp(chirp);
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.status("Error attempting to POST");
+  }
 });
 
 router.put("/:id", (req, res) => {
-  chirpsStore.UpdateChirp(req.params.id, req.body);
-  res.sendStatus(200);
+  try {
+    let id = req.params.id;
+    let chirp = req.body;
+    chirpsStore.UpdateChirp(id, chirp);
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.status("Error attempting to PUT");
+  }
 });
 
 router.delete("/:id", (req, res) => {
-  chirpsStore.DeleteChirp(req.params.id);
-  res.sendStatus(200);
+  try {
+    let id = req.params.id;
+    chirpsStore.DeleteChirp(id);
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.status("Error attempting to DELETE");
+  }
 });
 
 module.exports = router;
