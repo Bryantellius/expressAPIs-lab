55fe205cd10ce33a0f6c3d9225fc01470d240b92
Ben Bryant <brbryant2639@gmail.com>
2020-03-26 10:28:22 -0500

    created js file for methods

---
+++ client/methods/chirpMethods.js
@@ -0,0 +1,92 @@
+const deleteChirp = props => {
+  console.log(props.id);
+  $(`#${props.id}`).remove();
+  $.ajax({
+    url: `http://localhost:3000/api/chirps/${props.id}`,
+    type: "DELETE"
+  }).done(res => console.log(res));
+};
+
+const modalEdit = props => {
+  if ($("#editName").val() === "" || $("#editTextArea").val() === "") {
+    return null;
+  } else {
+    let editedChirp = {
+      name: $("#editName").val(),
+      text: $("#editTextArea").val()
+    };
+    console.log(editedChirp);
+    $.ajax({
+      url: `http://localhost:3000/api/chirps/${props}`,
+      type: "PUT",
+      contentType: "application/json",
+      data: JSON.stringify(editedChirp)
+    }).done(res => {
+      console.log(res);
+      loadChirps();
+    });
+  }
+  $("#editName").val("");
+  $("#editTextArea").val("");
+};
+
+const modalToggle = props => {
+  $("#modalSave").click(() => modalEdit(props.id));
+  console.log("before modalSave");
+};
+
+const addChirp = () => {
+  $("#displayBtn").click(() => {
+    if ($("#inputName").val() === "" || $("#chirpsTextArea").val() === "") {
+      $("#inputSection").prepend(
+        $(
+          "<h5 class='text-center bg-warning rounded shadow' id='inputWarning'>Input fields cannot be blank</h5>"
+        )
+      );
+    } else {
+      let chirp = {
+        name: $("#inputName").val(),
+        text: $("#chirpsTextArea").val()
+      };
+      console.log(chirp);
+      $.ajax({
+        url: "http://localhost:3000/api/chirps",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(chirp)
+      }).done(res => {
+        console.log(res);
+        console.log(`post: ${chirp}`);
+        loadChirps();
+      });
+
+      $("#inputWarning").remove();
+      $("#inputName").val("");
+      $("#chirpsTextArea").val("");
+    }
+  });
+};
+
+const loadChirps = () => {
+  $("#chirpsSection").empty();
+  $.ajax({
+    url: "http://localhost:3000/api/chirps",
+    type: "GET"
+  }).done(data => {
+    Object.entries(data).map(([key, value]) => {
+      if (key === "nextid") {
+        return null;
+      } else {
+        console.log(`${value.name}: ${value.text}, at ${key}`);
+        $("#chirpsSection").prepend(
+          `<div class='card p-3' id=${key}>
+                  <span class='delete' onclick={deleteChirp(this.parentNode)}>X</span>
+                  <h3 class='card-title'>${value.name}</h3>
+                  <p class='card-body'>${value.text}</p>
+                  <button class='btn btn-primary btn-sm w-25' onclick={modalToggle(this.parentNode)} data-target="#exampleModal" data-toggle="modal">Edit</button>
+                </div>`
+        );
+      }
+    });
+  });
+};
